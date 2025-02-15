f4988c45662ddee59eea1db2af8f85389a7efebb
suk-6 <me@suk.kr>
2025-02-16 03:08:58 +0900

    feat: implement GetUser decorator and update FavoriteController to use it for user authentication

--- src/modules/favorite/favorite.controller.ts
+++ src/modules/favorite/favorite.controller.ts
@@ -1,5 +1,7 @@
 import { Body, Controller, Delete, Get, Put, Query } from "@nestjs/common";
-import { ApiBody, ApiQuery, ApiTags } from "@nestjs/swagger";
+import { ApiBearerAuth, ApiBody, ApiQuery, ApiTags } from "@nestjs/swagger";
+
+import { GetUser } from "src/common/decorators/get.user.decorator";
 
 import {
   GetChannelsDTO,
@@ -20,26 +22,26 @@ export class FavoriteController {
   }
 
   @Get()
-  @ApiQuery({ name: "userId", required: true, type: String })
-  async getChannels(@Query("userId") userId: string): Promise<GetChannelsDTO> {
+  @ApiBearerAuth()
+  async getChannels(@GetUser() userId: string): Promise<GetChannelsDTO> {
     return await this.favoriteService.getChannels(userId);
   }
 
   @Put("/add")
-  @ApiQuery({ name: "userId", required: true, type: String })
   @ApiBody({ type: OneChannelDTO })
+  @ApiBearerAuth()
   async addChannel(
-    @Query("userId") userId: string,
+    @GetUser() userId: string,
     @Body() { channelId }: OneChannelDTO,
   ) {
     return await this.favoriteService.addChannel(userId, channelId);
   }
 
   @Delete("/remove")
-  @ApiQuery({ name: "userId", required: true, type: String })
   @ApiBody({ type: MultiChannelDTO })
+  @ApiBearerAuth()
   async removeChannel(
-    @Query("userId") userId: string,
+    @GetUser() userId: string,
     @Body() { channelIds }: MultiChannelDTO,
   ) {
     return await this.favoriteService.removeChannel(userId, channelIds);
